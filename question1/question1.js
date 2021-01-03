76a6fde0b742a9010d7dfc29a5933cc8610641c0
FatmaHamad-M1-56 <76535095+FatmaHamad-M1-56@users.noreply.github.com>
2021-01-03 21:45:24 +0300

    fatma

--- question1/question1.js
+++ question1/question1.js
@@ -1,1 +1,18 @@
 // 👇🏻 قم بحل السؤال الأول هنا
+function sayHello(name) {
+
+    if (name==null)
+   {
+  console.log(`ادخل الاسم`); 
+  }
+  
+  else if (name!=null)
+   {
+      console.log(`مرحبا ${name}`);
+  
+  }
+  
+  }
+  
+  sayHello();
+  sayHello(`فاطمة`);
